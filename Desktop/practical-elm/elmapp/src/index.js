c0c89a811d151b10c85d46e9653db67ad7e5964d
Ethan <theejdavidson@gmail.com>
2019-12-18 19:41:47 -0600

    elm lang slack folks rule

--- Desktop/practical-elm/elmapp/src/index.js
+++ Desktop/practical-elm/elmapp/src/index.js
@@ -2,9 +2,14 @@ import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
 
-Elm.Main.init({
-  node: document.getElementById('root')
-});
+let app = Elm.Main.init({
+  flags: {sessionId: localStorage.getItem("sessionId")} })
+  app.ports.saveSessionId.subscribe((sessionId) => {
+    if (sessionId === null) localStorage.removeItem("sessionId")
+    else
+                localStorage.setItem("sessionId", sessionId)
+    })
+    
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
